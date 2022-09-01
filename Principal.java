5720475aff2f9759a116f2e67b3da7421e398de7
Jxsst91 <111443236+jxsst91@users.noreply.github.com>
2022-09-01 12:45:24 +0000

    n

---
+++ Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("hola");
+        //variable
+        int var1=10;
+        System.out.println(var1);
+        var1++;
+        System.out.println(var1);
+        Persona p1=new Persona("Maria",12,"F","jocotitlan");
+        Estudiante e1=new Estudiante("juan",17,"M","atlacomulco","0123","paradigmas","B+");
+
+        e1.mostrarInfo();
+        p1.mostrarInfo();
+
+
+
+    }
+}
